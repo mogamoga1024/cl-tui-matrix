0b07c2556c10d89f01ae50f1cc13e0768d2d2c33
mogamoga <mogamoga.g1thu6@gmail.com>
2022-12-17 03:18:00 +0900

    a

--- main.lisp
+++ main.lisp
@@ -3,6 +3,23 @@
 
 (defun main ()
   (cl-setlocale:set-all-to-native)
-  (format t "hello~%"))
+  (charms:with-curses ()
+    (charms:disable-echoing)
+    (charms:enable-raw-input :interpret-control-characters t)
+    (charms:enable-non-blocking-mode charms:*standard-window*)
+
+    (loop :for c := (charms:get-char charms:*standard-window*
+                                     :ignore-error t)
+          :do (progn
+                ;; Redraw time
+                (charms:clear-window charms:*standard-window*)
+                ;(paint-time)
+                (charms:refresh-window charms:*standard-window*)
+
+                ;; Process input
+                (case c
+                  ((nil) nil)
+                  ;((#\Space) (start/stop/clear))
+                  ((#\q #\Q) (return)))))))
 
 
