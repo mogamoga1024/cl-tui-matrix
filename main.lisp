4fc1c8c5d697d3b3a1afbcf03b6a7d72ade8fb3b
mogamoga <mogamoga.g1thu6@gmail.com>
2022-12-17 07:28:19 +0900

    a

--- main.lisp
+++ main.lisp
@@ -22,26 +22,26 @@
       (charms/ll:init-color (+ 8 i) 0 (* (floor 1000 +kiri-kawa-len+) (- +kiri-kawa-len+ i)) 0)
       (charms/ll:init-pair (1+ i) (+ 8 i) charms/ll:COLOR_BLACK))
 
-    ; x y pair-id char-idx
+    ; x y color-pair-id char-idx
     (push '(0 0 1 0) *char-list*)
 
-    (loop :with y := 0
-          :do (progn
-                (charms:clear-window charms:*standard-window*)
+    (loop (setf y (second (car *char-list*))) ; TODO
+          (setf color-pair-id (third (car *char-list*))) ; TODO
+          
+          (charms:clear-window charms:*standard-window*)
 
-                (dotimes (i (if (< y +kiri-kawa-len+) y +kiri-kawa-len+))
-                  (charms/ll:wattron (charms::window-pointer charms:*standard-window*) (charms/ll:color-pair (1+ i)))
-                  (charms:write-string-at-point charms:*standard-window* *kiri-kawa* 0 (- y i))
-                  (charms/ll:wattroff (charms::window-pointer charms:*standard-window*) (charms/ll:color-pair (1+ i))))
+          (charms/ll:wattron (charms::window-pointer charms:*standard-window*) (charms/ll:color-pair color-pair-id))
+          (charms:write-string-at-point charms:*standard-window* *kiri-kawa* 0 y)
+          (charms/ll:wattroff (charms::window-pointer charms:*standard-window*) (charms/ll:color-pair color-pair-id))
 
-                (charms:refresh-window charms:*standard-window*)
+          (charms:refresh-window charms:*standard-window*)
 
-                (multiple-value-bind (width height)
-                    (charms:window-dimensions charms:*standard-window*)
-                  (setf y (mod (1+ y) height)))
+          (multiple-value-bind (width height)
+              (charms:window-dimensions charms:*standard-window*)
+            (setf (second (car *char-list*)) (mod (1+ y) height))) ; TODO
 
-                (case (charms:get-char charms:*standard-window* :ignore-error t)
-                  ((#\q #\Q) (return)))
-                (sleep 0.1)))))
+          (case (charms:get-char charms:*standard-window* :ignore-error t)
+            ((#\q #\Q) (return)))
+          (sleep 0.1))))
 
 
