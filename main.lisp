23eaeda850cbeda36e4c04854afacd0e197c2f25
mogamoga <mogamoga.g1thu6@gmail.com>
2022-12-18 02:48:35 +0900

    golf

--- main.lisp
+++ main.lisp
@@ -1,13 +1,48 @@
 (ql:quickload '(:cl-charms :cl-setlocale))
 (defpackage :main (:use :cl :charms :charms/ll :cl-setlocale)
 (:shadow :curses-version))(in-package :main)
-(defun main ()(set-all-to-native)(with-curses ()(let* ((a "K1Я1+aИkawa11ya++a-")
-(b 19)(c)(d)(e *standard-window*)(f (charms::window-pointer e)))(disable-echoing)
-(enable-raw-input)(enable-non-blocking-mode e)(start-color)(dotimes (i b)(init-color
-(+ 8 i) 0 (* (floor 1000 b) (- b i)) 0)(init-pair (1+ i) (+ 8 i) 0))(push '(0 0 1 0) c)
-(loop (multiple-value-bind (width height)(window-dimensions e)(clear-window e)(dolist
-(k c)(destructuring-bind (x y g h) k (let ((i (color-pair g)))(wattron f i)
-(write-string-at-point e (subseq a h (1+ h)) x y)(wattroff f i))(if (< (1+ y) height)
-(push (list x (1+ y) g (mod (1+ h) b)) d))(if (and (= y 0) (< g b))(push (list x 0 (1+ g)
-h) d))))(if (= (random 2) 0)(push (list (random (1- width)) 0 1 (random b)) d))(setf c d
-d nil)(refresh-window e)(case (get-char e :ignore-error t)((#\q #\Q) (return)))(sleep 0.1))))))
+
+(defun main ()
+  (set-all-to-native)
+  (with-curses ()
+    (let* ((kiri-kawa "K1Я1+aИkawa11ya++a-")
+           (kiri-kawa-len (length kiri-kawa))
+           (char-list)
+           (new-char-list)
+           (win *standard-window*)
+           (win-ptr (charms::window-pointer win)))
+      (disable-echoing)
+      (enable-raw-input)
+      (enable-non-blocking-mode win)
+
+      (start-color)
+      (dotimes (i kiri-kawa-len)
+        (init-color (+ 8 i) 0 (* (floor 1000 kiri-kawa-len) (- kiri-kawa-len i)) 0)
+        (init-pair (1+ i) (+ 8 i) 0))
+
+      (loop
+        (multiple-value-bind (width height)
+            (window-dimensions win)
+          (clear-window win)
+
+          (dolist (char char-list)
+            (destructuring-bind (x y color-pair-id char-idx) char
+              (wattron win-ptr (color-pair color-pair-id))
+              (write-string-at-point win (subseq kiri-kawa char-idx (1+ char-idx)) x y)
+              (wattroff win-ptr (color-pair color-pair-id))
+              (when (< (1+ y) height)
+                (push (list x (1+ y) color-pair-id (mod (1+ char-idx) kiri-kawa-len)) new-char-list))
+              (when (and (= y 0) (< color-pair-id kiri-kawa-len))
+                (push (list x 0 (1+ color-pair-id) char-idx) new-char-list))))
+          (let ((new-x (random (1- width))))
+            (when (every #'(lambda (lst) (or (/= (car lst) new-x) (/= (cadr lst) 0))) new-char-list)
+              (push (list new-x 0 1 (random kiri-kawa-len)) new-char-list)))
+          (setf char-list new-char-list new-char-list nil)
+
+          (refresh-window win)
+          
+          (case (get-char win :ignore-error t)
+            ((#\q #\Q) (return)))
+          (sleep 0.1))))))
+
+
