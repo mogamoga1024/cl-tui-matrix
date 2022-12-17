963f437f7587858672684822cafb6e3f2f1a9585
mogamoga <mogamoga.g1thu6@gmail.com>
2022-12-17 16:06:21 +0900

    できた

--- main.lisp
+++ main.lisp
@@ -9,6 +9,7 @@
 (defconstant +kiri-kawa-len+ (length *kiri-kawa*))
 (defparameter *char-list* nil)
 (defparameter *new-char-list* nil)
+(defparameter *frame* 0)
 
 (defun main ()
   (cl-setlocale:set-all-to-native)
@@ -23,10 +24,7 @@
       (charms/ll:init-color (+ 8 i) 0 (* (floor 1000 +kiri-kawa-len+) (- +kiri-kawa-len+ i)) 0)
       (charms/ll:init-pair (1+ i) (+ 8 i) charms/ll:COLOR_BLACK))
 
-    ; x y color-pair-id char-idx
     (push '(0 0 1 0) *char-list*)
-    ; (push '(5 0 1 3) *char-list*)
-    ; (push '(10 0 1 6) *char-list*)
 
     (loop
       (multiple-value-bind (width height)
@@ -39,17 +37,14 @@
             ;; xが(1- width)かつyが(1- height)だと落ちる
             (charms:write-string-at-point charms:*standard-window* (subseq *kiri-kawa* char-idx (1+ char-idx)) x y)
             (charms/ll:wattroff (charms::window-pointer charms:*standard-window*) (charms/ll:color-pair color-pair-id))
+            ;; 一段下げる
             (if (< (1+ y) height)
                 (push (list x (1+ y) color-pair-id (mod (1+ char-idx) +kiri-kawa-len+)) *new-char-list*))
             (if (and (= y 0) (< color-pair-id +kiri-kawa-len+))
-                (push (list x 0 (1+ color-pair-id) char-idx) *new-char-list*))
-            (if (and (< (length *char-list*) (* 50 +kiri-kawa-len+)) (= (random 100) 0))
-                (push (list (random (1- width)) 0 1 (random +kiri-kawa-len+)) *new-char-list*))
-            ; (if (= (random 100) 0)
-            ;     (push (list (random width) 0 1 (random +kiri-kawa-len+)) *new-char-list*))
-            
-            
-            ))
+                (push (list x 0 (1+ color-pair-id) char-idx) *new-char-list*))))
+        ;; 新しいやつ生成
+        (if (= (random 2) 0)
+            (push (list (random (1- width)) 0 1 (random +kiri-kawa-len+)) *new-char-list*))
         (setf *char-list* *new-char-list* *new-char-list* nil)
 
         (charms:refresh-window charms:*standard-window*)
