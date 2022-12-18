a1f5b9d4ec1baee990a0857883f26d99d2d111ce
mogamoga <mogamoga.g1thu6@gmail.com>
2022-12-18 13:47:11 +0900

    a

--- main.lisp
+++ main.lisp
@@ -4,7 +4,7 @@
 (defun main()(set-all-to-native)(with-curses()(let*((e *standard-window*)(c)(d)
 (a "K1Я1+aИkawa11ya++a-")(b(length a))(f(charms::window-pointer e))(g))
 (disable-echoing)(enable-raw-input)(enable-non-blocking-mode e)(start-color)
-(dotimes(i b)(init-color(+ 8 i)0(*(floor 1000 b)(- b i))0)(init-pair(1+ i)(+ 8 
+(dotimes(i b)(init-color(+ 8 i)0(*(floor 1000 b)(- b i))0)(init-pair(1+ i)(+ 8
 i)0))(loop(multiple-value-bind(h j)(window-dimensions e)(clear-window e)(dolist
 (m c)(destructuring-bind(x y k l)m(let((o(color-pair k)))(wattron f o)
 (write-string-at-point e(subseq a l(1+ l))x y)(wattroff f o))(if(<(1+ y)j)(push
